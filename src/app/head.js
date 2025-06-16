7fbfec03957e34636252902870971dbdc79f7a75
OLIVER-PC\wangy <2995306790@qq.com>
2025-06-16 22:07:45 +0800

    改了改，大概有了个样

---
+++ src/app/head.js
@@ -0,0 +1,17 @@
+export default function Head() {
+  return (
+    <div id="head">
+      <h1 id="title">电子木鱼</h1>
+      <p id="title-sub">王中王荣誉出品</p>
+    </div>
+  );
+}
+export function Foot(){
+  return(
+    <footer id="foot">
+      <p className="foot-text">我的博客</p>
+      <p className="foot-text">联系我</p>
+      <p className="foot-text">关于本软件</p>
+    </footer>
+  )
+}
